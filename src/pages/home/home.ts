20a99c2c32927319c31af5c6539c41bb9982ffca
AnorldMaatjie <maatjieac@gmail.com>
2018-08-14 13:18:24 +0200

    first commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { LogInPage } from '../log-in/log-in';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+
+  Next(){
+    this.navCtrl.push(LogInPage)
+  }
+
+}
